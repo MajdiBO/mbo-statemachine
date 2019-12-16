5107dfbe58bacde0629970488c946c010b867ade
MajdiBO <majdi.ben.ouaghrem@gmail.com>
2019-12-16 01:27:51 +0100

    add publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,23 @@ val jupiterVersion = "5.5.2"
 
 plugins {
     kotlin("jvm") version "1.3.60"
+    `maven-publish`
 }
 
 group = "io.mbo.labs"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
+}
+
+publishing {
+    publications {
+        register("mavenKotlin", MavenPublication::class) {
+            artifact("build/libs/${rootProject.name}-$version.jar")
+        }
+    }
 }
 
 dependencies {
